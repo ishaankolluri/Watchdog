6e0258928e210c9c4a0d4c97fde23f7e5d709fa2
ishaankolluri <ishaankolluri@gmail.com>
2016-10-27 01:35:12 -0400

    Models plus cleanup

--- simulator/views.py
+++ simulator/views.py
@@ -1,32 +1,23 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.views import generic
-from django.contrib.auth.decorators import login_required
-from simulator.forms import UserForm
 from django.contrib.auth.models import User
+from django.shortcuts import render
+
+from simulator.forms import UserForm
 
 
-
-# class LoginView(generic.TemplateView):
-#     template_name = "login.html"
-
-# @login_required(login_url="login/")
 def home(request):
-	return render(request, 'home.html')
+    return render(request, 'home.html')
 
 
 def login(request):
-	return render(request, 'login.html')
+    return render(request, 'login.html')
+
 
 def signup(request):
     if request.method == "POST":
-
         form = UserForm(request.POST)
         if form.is_valid():
             user = User.objects.create_user(**form.cleaned_data)
-            #login(user)
-            # return HttpResponseRedirect('home.html')
+            user.save()
             return render(request, 'home.html')
     else:
         form = UserForm()

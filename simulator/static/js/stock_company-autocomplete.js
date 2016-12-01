9224208a998763cbcf79755191b7bbc7e734ec3c
Ishaan Kolluri <ishaankolluri@gmail.com>
2016-11-30 20:34:39 -0500

    Delete garbage

--- simulator/static/js/stock_company-autocomplete.js
+++ simulator/static/js/stock_company-autocomplete.js
@@ -6760,8 +6760,7 @@ function getstockdata(stocksymbol){
 	document.getElementById("stockgraph").className="dog_gif";
 	//change the image back to dog gif for the next stock search
 	$("#stockgraph").attr("src","../static/loading-dog.gif");
-    })
-
+ })
  }).fail(function(error){
     console.log(error);
 });

257795500628d226e1a3e23f3ed6869ebc064b55
Ishaan Kolluri <ishaankolluri@gmail.com>
2016-12-01 16:43:15 -0500

    Fixed >500 purchase on first position

--- simulator/views.py
+++ simulator/views.py
@@ -90,15 +90,19 @@ def market_execution(request):
         pos_list = Position.objects.filter(user=user, instrument=ins)
         if pos_list.count() == 0:
             if execution == "buy":
-                Position.objects.create(
-                    user=user,
-                    instrument=ins,
-                    symbol=symbol,
-                    price_purchased=last_trade_price,
-                    quantity_purchased=quantity,
-                    date_purchased=datetime.datetime.now(),
-                )
-                message = "You have placed a new market buy"
+                if quantity > 500:
+                    success = False
+                    message = "Please buy less than 500 stocks at a time."
+                else:
+                    Position.objects.create(
+                        user=user,
+                        instrument=ins,
+                        symbol=symbol,
+                        price_purchased=last_trade_price,
+                        quantity_purchased=quantity,
+                        date_purchased=datetime.datetime.now(),
+                    )
+                    message = "You have placed a new market buy"
             else:
                 # Selling a stock you don't own.
                 message = "You cannot sell a stock you do not own."
